ac0b66ffa41e824bdc928f8edff16dfb12ffe59d
João <joaopedrocararinerangel@gmail.com>
2026-05-27 13:00:07 -0300

    refactor: ajusta navegação da tela de pedidos

--- frontend/src/app/(private)/(tabs)/orders.tsx
+++ frontend/src/app/(private)/(tabs)/orders.tsx
@@ -1,11 +1,10 @@
 import { View, Text } from "react-native";
+import OrderView from "../../../viewModels/Order/Order.view";
+import useOrderViewModel from "../../../viewModels/Order/Order.viewModel";
 
 export default function Orders() {
+    const props = useOrderViewModel();
     return (
-        <View>
-            <Text>
-                ORDERS
-            </Text>
-        </View>
+        <OrderView {...props} />
     )
 }
