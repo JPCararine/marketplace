ac60e1aea96eb8b6b6779240eb42db9e799ea743
João <joaopedrocararinerangel@gmail.com>
2026-05-27 18:33:48 -0300

    refactor: reorganiza consultas e listagem de pedidos

--- frontend/src/shared/queries/order/use-getOrder.queries.ts
+++ frontend/src/shared/queries/order/use-getOrder.queries.ts
@@ -1,5 +1,5 @@
 import { useQueries, useQuery } from "@tanstack/react-query";
-import * as orderService from "../services/order.service";
+import * as orderService from "../../services/order.service";
 export default function orderQueries () {
     
     return useQuery({
--- frontend/src/viewModels/Order/Order.viewModel.ts
+++ frontend/src/viewModels/Order/Order.viewModel.ts
@@ -1,13 +1,15 @@
-import orderQueries from "../../shared/queries/use-getOrder.queries"
+import { useQueries } from "@tanstack/react-query";
+import orderQueries from "../../shared/queries/order/use-getOrder.queries"
 
 
 export default function useOrderViewModel() {
-    const {data, isLoading} = orderQueries();
+    const { data, isLoading }  = orderQueries();
+    
 
 
     return {
         data,
-        isLoading,
+        isLoading
     }
         
     
